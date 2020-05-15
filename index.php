601db865038d9542a30260af35e948d1a642151c
KrulYuno <giogulane.wineffgulane@gmail.com>
2020-05-16 00:48:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-$user = $_POST['user_login'];
+#$user = $_POST['user_login'];
+$user = '';
 $map = '';
 
 parse_str($_SERVER["QUERY_STRING"]);
+$user = htmlspecialchars($user);
 if ($map === 'saw'){
 	$trapLocations = range(0, 25);
 	shuffle($trapLocations);
 	for ($i=0; $i <= 25; $i++){
-		file_put_contents('seed/'.$user.'_'.$i, $trapLocations[$i]);
+		file_put_contents('seed/'.$user.'_'.$i, strval($trapLocations[$i]));
 	}
 	
-	file_put_contents('seed/'.$user.'_seed', implode('/',$trapLocations));
+#	file_put_contents('seed/'.$user.'_seed', implode('|',$trapLocations));
+	file_put_contents('seed/'.$user.'_seed', json_encode($trapLocations));
 }
 
 
